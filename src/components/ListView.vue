0e655ce9d5bf4c389c8bf561d3bf833dc0bb81f8
ys2cdh <ys2cdh@douzone.com>
2024-04-11 18:18:59 +0900

    기본 리스트 조회 완성

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -2,47 +2,82 @@
 <template>
   <div>
     <h1>List of Messages</h1>
-    <ul>
-      <li v-for="message in displayedMessages" :key="message.messageId">
-        <div>{{ message.from }} -> {{ message.to[0].email  }}  {{ message.subject }}</div>
-        
-       
-      </li>
-    </ul>
-    <button @click="prevPage()" :disabled="currentPage === 1">Previous</button>
-    <button @click="nextPage()" :disabled="currentPage === totalPages">Next</button>
+    <table>
+      <thead>
+        <tr>
+          <th>보낸 사람</th>
+          <th>받는 사람</th>
+          <th >제목</th>
+          <th >시간</th>
+          <th >크기</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="message in displayedMessages" :key="message.id">
+          <td>{{ message.from }}</td>
+          <td>{{ message.to[0].email }}</td>
+          <td>
+            <!-- 조건부 렌더링 -->
+            <span v-if="message.attach">
+              <img :src="require('../img/ico_file.gif')" alt="clip icon" width="7" height="13">
+            </span>
+            {{ message.subject }}
+          </td>
+          <td>{{ message.date }}</td>
+          <td>{{ message.size }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="pagination">
+      <button  @click="prevPage()" :disabled="currentPage === 1">&#60;</button>
+      <button v-for="pageNumber in displayedPages" :key="pageNumber" @click="goToPage(pageNumber)" @input="handleInput">
+        {{ pageNumber }}
+      </button>
+      <button @click="nextPage()" :disabled="currentPage === totalPages">&#62;</button>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import { watch  } from 'vue'
 
-export default {
+
+export default {    
   data() {
     return {
-      messages: [],
-      currentPage: 1,
+      messages: [],  
+      currentPage:1,    
+      itemsPerPage: 10, // 페이지당 표시할 항목 수      
+      totalItems: 0, // 전체 항목 수
       pageSize: 10
     };
   },
   computed: {
     totalPages() {
-      return Math.ceil(this.messages.length / this.pageSize);
+      return Math.ceil(this.totalItems / this.itemsPerPage);
     },
     displayedMessages() {
-      const start = (this.currentPage - 1) * this.pageSize;
-      const end = start + this.pageSize;
-      return this.messages.slice(start, end);
-    }
+      return this.messages;
+    },
+    displayedPages() {
+      // 현재 페이지를 중심으로 앞뒤로 5개씩 총 10개의 페이지 번호를 반환
+      const startPage = Math.max(1, this.currentPage - 5);
+      const endPage = Math.min(this.totalPages, startPage + 9);
+      return Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
+    },
   },
   mounted() {
-    this.fetchMessages();
+    this.fetchMessages();    
+    //아래 watch는 작동하는 코드 임
+    watch(() => this.currentPage, (current, previous) => { console.log('[count] watch => ', current, previous) });
   },
   methods: {
     fetchMessages() {
       axios.get(`http://localhost:8080/allList.jsp?from=${this.currentPage}&size=${this.pageSize}`)
         .then(response => {
           this.messages = response.data.data;
+          this.totalItems = response.data.total;
         })
         .catch(error => {
           console.error('Error fetching messages:', error);
@@ -62,11 +97,40 @@ export default {
         this.currentPage--;
         this.fetchMessages();
       }
-    }
+    },
+    goToPage(pageNumber) {
+      this.currentPage = pageNumber;
+      this.fetchMessages();
+    },
+    handleInput (current, previous) {
+      console.log('[count] handleInput => ', current, previous) 
+    },
   }
 }
 </script>
 
 <style>
-/* CSS 스타일을 여기에 추가할 수 있습니다. */
+thead {
+  background-color: #333;
+  color: white;
+}
+.pagination {
+  margin-top: 10px;
+}
+
+.pagination button {
+  margin: 0 5px;
+  border: none; /* 테두리 제거 */
+  background: none; /* 배경 제거 */
+  cursor: pointer;
+}
+
+.pagination button:hover {
+  text-decoration: underline; /* 마우스 호버 시 밑줄 추가 */
+}
+
+.pagination button:focus {
+  font-weight: bold; 
+  color: blue; /* 선택된 페이지 버튼의 배경색을 파란색으로 변경 */
+}
 </style>
